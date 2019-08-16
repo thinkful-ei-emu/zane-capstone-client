8fd1f3c98fd5186f565276a7f481f97cc1457fc0
Zane Kuehn <zanekuehn@yahoo.com>
2019-08-16 13:25:31 -0500

    Change to Readme

--- src/Components/LandingPageComponents/Intro-Section.js
+++ src/Components/LandingPageComponents/Intro-Section.js
@@ -8,6 +8,7 @@ export default function IntroSection(){
       <p className='introparagraph'>Zap Inventory is a simple straightforward way to keep track of your inventory.This user-friendly app is 
         meant for anyone that just needs to keep track of their items. Get Started by clicking the links below.
       </p>
+      <p className='introparagraph'>To see the app in action without making a user account use the credentials. <br/>UserName: testuser<br/> Password: Password321!</p>
     </section>
   )
 }
